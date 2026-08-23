4d7f6ca85f87e45d7fc2b319aae9b0dc6f2f49f8
ribka <micko.eleonora@gmail.com>
2026-08-23 23:39:27 +0300

    part-3-lifecycle

--- test/setup.ts
+++ test/setup.ts
@@ -1,1 +1,11 @@
 import 'reflect-metadata';
+import { vi, beforeEach, afterEach } from 'vitest';
+
+beforeEach(() => {
+    vi.spyOn(console, 'log').mockImplementation(() => {});
+    vi.spyOn(console, 'error').mockImplementation(() => {});
+});
+
+afterEach(() => {
+    vi.restoreAllMocks();
+});
